e44f7cee0bc75904ba3a83eb1442b3a5c12dbb84
gunwunbun <jvanhouts@protonmail.com>
2024-01-24 10:00:52 +0100

    Initial Commit

---
+++ EchoDiary/EchoDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoDiaryApp.swift
+//  EchoDiary
+//
+//  Created by JVH on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EchoDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
